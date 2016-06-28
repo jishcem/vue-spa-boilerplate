8bb6c19fbaef9ebcb95086d0bcfd21a39364f983
jishcem <jishcem@gmail.com>
2016-06-28 23:36:42 +0530

    reset password

--- src/components/ResetPassword.vue
+++ src/components/ResetPassword.vue
@@ -4,16 +4,17 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-6 col-sm-offset-3 text-center">
+          <errors :errors="errors"></errors>
           <form v-on:submit.prevent="submit" class="form-signin">
             <h2 class="form-signin-heading">Reset your Password ?</h2>
             <label for="inputEmail" class="sr-only">Email address</label>
-            <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" autofocus>
             <br>
             <label for="inputPassword" class="sr-only">Password</label>
-            <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password">
             <br>
             <label for="inputPasswordConfirm" class="sr-only">Confirm Password</label>
-            <input v-model="password_confirm" type="password" id="inputPasswordConfirm" class="form-control" placeholder="Confirm Password" required>
+            <input v-model="password_confirm" type="password" id="inputPasswordConfirm" class="form-control" placeholder="Confirm Password">
             <br>
             <button class="btn btn-lg btn-primary btn-block password-button" data-loading-text="Sending request..." type="submit">Submit</button>
           </form>
@@ -28,9 +29,12 @@
   import AppHeader from '../components/template/AppHeader'
   import AppFooter from '../components/template/AppFooter'
   import NProgress from 'nprogress'
+  import Utils from '../utils'
+  import Errors from './template/Errors.vue'
+
   export default {
     components: {
-      AppHeader, AppFooter
+      AppHeader, AppFooter, Errors
     },
 
     ready () {
@@ -43,7 +47,8 @@
         email: null,
         token: null,
         password: null,
-        password_confirm: null
+        password_confirm: null,
+        errors: null
       }
     },
 
@@ -57,17 +62,19 @@
             email: this.email,
             token: this.token,
             password: this.password,
-            password_confirm: this.password_confirm
+            password_confirmation: this.password_confirm
           }
         ).then((response) => {
           window.$('.password-button').button('reset')
           console.log(response)
           NProgress.done()
+          this.$router.go('/login')
         }).catch((error) => {
           console.log(error)
           this.error = true
           window.$('.password-button').button('reset')
           NProgress.done()
+          this.errors = Utils.getErrorArray(error.data.errors)
         })
       }
     }
